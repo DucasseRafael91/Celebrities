2a991f384b7056ca9d394ad1d18bb4ee40cd8a75
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-04 10:28:49 +0100

    Creation des invités

--- Celebrities.java
+++ Celebrities.java
@@ -1,5 +1,28 @@
 package Celebrities;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Celebrities {
 
+    public static void main(String[] args) {
+        
+        Map<String, int[]> invitees = new LinkedHashMap<>(); 
+        invitees.put("Albert", new int[] {2, 5, 6});
+        invitees.put("Bénédicte", new int[] {3, 5, 6});
+        invitees.put("Christophe", new int[] {2, 4, 5, 6});
+        invitees.put("Delphine", new int[] {1, 2, 5, 6, 8});
+        invitees.put("Edouard", new int[] {6});
+        invitees.put("Françoise", new int[] {5});
+        invitees.put("Gaston", new int[] {2, 3, 5, 6, 8});
+        invitees.put("Heloïse", new int[] {2, 4, 5, 6, 7});
+    
+        for (Map.Entry<String, int[]> invite : invitees.entrySet()) {
+            System.out.print(invite.getKey() + " ");
+            for (int num : invite.getValue()) {
+                System.out.print(num + " ");
+            }
+            System.out.println(); 
+        }
+    }
 }
